43c38fc31c9bb9314dcce3ce73fc1e02acc45298
anirudh <62985710+anirudhd575@users.noreply.github.com>
2023-01-16 01:53:27 -0800

    lab0

--- greetings.c
+++ greetings.c
@@ -1,15 +1,27 @@
+/*
+ * Author: Anirudh Devanand
+ * Student Number: 38304895
+ * Lab Section: L2M
+ * Date: 16 January 2023
+ * Purpose: Prompts the user for his/ her name and prints
+ * a welcome message on the screen.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /*constants*/
 #define MAX_NAME_LENGTH 100
 
-int main(void){
+
+
+int main(void)
+{
     char name[MAX_NAME_LENGTH];
     printf("Please enter your first name: ");
-    scanf("%s", name);
-    printf("\nHello, %s, welcome to APSC160!\n\n", name);
+    scanf("%s", &name);
+    printf("\nHello, %s, welcome to APSC 160!\n\n", name);
 
-    system("PAUSE");
     return 0;
 }
