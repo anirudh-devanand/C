74f65332d022955dddd01427c06eb4ad694cd15a
anirudh <62985710+anirudhd575@users.noreply.github.com>
2023-01-16 01:56:13 -0800

    formatted

--- greetings.c
+++ greetings.c
@@ -7,20 +7,17 @@
  * a welcome message on the screen.
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
 /*constants*/
 #define MAX_NAME_LENGTH 100
 
-
-
-int main(void)
-{
+int main(void){
     char name[MAX_NAME_LENGTH];
+
     printf("Please enter your first name: ");
-    scanf("%s", &name);
+    scanf("%s", name);
     printf("\nHello, %s, welcome to APSC 160!\n\n", name);
 
     return 0;
