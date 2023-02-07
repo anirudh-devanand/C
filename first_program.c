c27c30cbb3832c5f8a2e8c89537536ebac735064
anirudh <62985710+anirudhd575@users.noreply.github.com>
2023-02-06 20:22:47 -0800

    shite

--- first_program.c
+++ first_program.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printTri(int n, char c){
-    for (int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            printf("%c", c);
-        }
-        printf("\n");
-    }
-}
-
 int main(void){
-    int n;
-    char c;
-    scanf("%d %c", &n, &c);
-    printTri(n,c);
+    int n=10;
+    if (n<2)
+    n=2;
+    printf("%d",n);
 }
