45c5ff12688bf38d6816562ff9a6fafcce6505e7
anirudh <62985710+anirudhd575@users.noreply.github.com>
2023-03-16 17:45:55 -0700

    practice

---
+++ APSC160/W10/practice.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void printStars( int numStars );
+void printRect(int width, int height);
+void printPic( int size);
+
+
+void main(){
+    printPic(3);
+}
+
+
+
+
+/*
+int countThreshold(int data[], int size,int threshold){
+    int count=0;
+    for(int index =0; index <size; index++){
+        if(data[index]>threshold){
+            count++;
+        }
+        return count;
+    }
+    
+}
+
+int arrEqual(int data1[], int data2[],int size){
+
+    int count=0;
+    int result=1;
+
+    for(int index =0; index <size; index++){
+        if(data1[index]=data2[index]){
+            count++;
+        }
+        else{
+            result = -1;
+            break;
+        }
+    }
+
+    return result;
+}
+
+
+int maxDiff(int data[],int size){
+    int maxDiff=0;
+    for(int index=0; index<size; index++){
+        int tempDiff=abs(data[index+1]-data[index]);
+        if(tempDiff>maxDiff){
+            maxDiff=tempDiff;
+        }
+    }
+    
+    return maxDiff;
+}
+
+*/
+
+
+
+
+
+/* 
+ * Prints a row of stars on the screen with a space after  
+ * every star. Does not print '\n' at the end of the row. 
+ * Param: numStars - the number of stars to print 
+ */ 
+void printStars( int numStars ){
+    for (int i =0; i<numStars;i++){
+        printf("* ");
+    }
+}
+
+
+void printRect(int width, int height){
+    for (int i=0;i<height;i++){
+        printStars(width);
+        printf("\n");
+    }
+}
+
+void printPic( int size){
+    for(int i=size; i>0;i--){
+        printRect((size*i),size);
+    }
+}
+
+
+
+
